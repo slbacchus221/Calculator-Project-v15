cc721e8d84fb45384a5753d9e573d17057572583
slbacchus221 <slbacchus221@gmail.com>
2024-04-16 09:32:34 -0400

    calculator project

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// 
+let result = document.getElementById('result');
+
+function number(value) {
+  result.value += value;
+}
+
+function clearScreen() {
+  result.value = '';
+}
+
+function calculate() {
+  try {
+    result.value = eval(result.value);
+  } catch(error) {
+    result.value = 'Error';
+  }
+}
